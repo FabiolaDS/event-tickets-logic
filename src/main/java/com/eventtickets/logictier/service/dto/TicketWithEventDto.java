a32bcc417eaa1c387a081874472f6ee45d324b23
Fabiola <285055@via.dk>
2021-11-17 18:45:36 +0100

    Implement TicketWithEventDto

---
+++ src/main/java/com/eventtickets/logictier/service/dto/TicketWithEventDto.java
@@ -0,0 +1,19 @@
+package com.eventtickets.logictier.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+public class TicketWithEventDto {
+//    private LocalDateTime ticketTimestamp;
+    private String ticketNumber ;
+    private String nameOfEvent;
+    private LocalDateTime dateTimeOfEvent;
+    private String location;
+    private double price;
+    private int numberOfTickets;
+    private String thumbnail;
+
+}
